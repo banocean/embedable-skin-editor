f2921e9e71f63f0adf9af0ca56895cbd3d01b11b
ocean <47253870+banocean@users.noreply.github.com>
2025-09-21 15:25:02 +0200

    remove ui

--- src/ui/main.js
+++ src/ui/main.js
@@ -9,9 +9,6 @@ import "./misc/skin_2d";
 
 import { css, html, LitElement, render, unsafeCSS } from "lit";
 import Editor from "../editor/main";
-import Toolbar from "./tools/toolbar";
-import LayerList from "./layers/layer_list";
-import Config from "./config/main";
 import PersistenceManager from "../persistence";
 import { getFocusedElement, isKeybindIgnored } from "../helpers";
 import Modal from "./misc/modal";
@@ -315,9 +312,6 @@ class UI extends LitElement {
 
     this.persistence = new PersistenceManager("ncrs-ui");
     this.editor = new Editor;
-    this.toolbar = new Toolbar(this);
-    this.layers = new LayerList(this);
-    this.config = new Config(this);
 
     this.exportModal = this._setupModal("export-form");
     this.galleryModal = this._setupGalleryModal();
@@ -325,6 +319,8 @@ class UI extends LitElement {
     this._setEditorTheme();
     this._setFullscreen();
     this._setupEvents();
+
+    this.classList.replace("minimized", "fullscreen");
   }
   currentLayer;
 
@@ -441,22 +437,12 @@ class UI extends LitElement {
   render() {
     return html`
       <div id="main">
-        ${this.config}
-        ${this.toolbar}
         <div id="editor">
           ${this.editor}
-          ${this._filtersWarning()}
-          ${this._bgToggle()}
-          ${this._fullscreenToggle()}
-        </div>
-        <div id="layers">
           ${this._historyButtons()}
-          ${this.layers}
         </div>
         ${this._setupColorCheckModal()}
       </div>
-      ${this.exportModal}
-      ${this.galleryModal}
       <slot name="footer"></slot>
     `;
   }
@@ -499,30 +485,6 @@ class UI extends LitElement {
     }
   }
 
-  _filtersWarning() {
-    const filterIcon = html`
-      <svg data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" stroke-linecap="round" stroke-linejoin="round"></path>
-      </svg>
-    `;
-    const eyeIcon = html`
-      <svg data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" stroke-linecap="round" stroke-linejoin="round"></path>
-      </svg>
-    `
-
-    return html`
-      <div id="filters-warning" class="warning">
-        ${filterIcon}
-        Colors drawn on the current layer will appear altered by filters.
-      </div>
-      <div id="layer-warning" class="warning">
-        ${eyeIcon}
-        Current layer is hidden, and cannot be edited.
-      </div>
-    `;
-  }
-
   _setEditorTheme() {
     if (
       !this.classList.contains("editor-dark") ||
@@ -537,29 +499,6 @@ class UI extends LitElement {
   _setFullscreen() {
     const fullscreen = "minimized";
     this.classList.add(fullscreen);
-  }
-
-  _bgToggle() {
-    return html`
-      <button id="themeSwitch" @click=${this.toggleEditorBackground}>
-        <ncrs-icon title="Switch to dusk mode." icon="dusk-mode" color="#ffffff66" class="dark"></ncrs-icon>
-        <ncrs-icon title="Switch to light mode." icon="light-mode" color="#ffffff66" class="gray"></ncrs-icon>
-        <ncrs-icon title="Switch to dark mode." icon="dark-mode" color="#00000088" class="light"></ncrs-icon>
-      </button>
-    `
-  }
-
-  _fullscreenToggle() {
-    return html`
-      <button id="fullscreenSwitchLightMode" @click=${this.toggleFullscreen}>
-        <ncrs-icon title="Switch to Fullscreen." icon="fullscreen" color="#00000088" class="minimized"></ncrs-icon>
-        <ncrs-icon title="Minimize." icon="minimize" color="#00000088" class="fullscreen"></ncrs-icon>
-      </button>
-      <button id="fullscreenSwitch" @click=${this.toggleFullscreen}>
-        <ncrs-icon title="Switch to Fullscreen." icon="fullscreen" color="#ffffff66" class="minimized"></ncrs-icon>
-        <ncrs-icon title="Minimize." icon="minimize" color="#ffffff66" class="fullscreen"></ncrs-icon>
-      </button>
-    `
   }
 
   _historyButtons() {
