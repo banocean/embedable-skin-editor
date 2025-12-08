cf677379c95e8a74fd6d9040b69456185cd9b57a
Moxvallix <dev@moxvallix.com>
2025-12-08 16:01:15 +1030

    fix: remove mobile from vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        mobile: resolve(__dirname, '/mobile/index.html'),
       },
     },
   },
