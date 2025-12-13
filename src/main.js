0be59ad43700f9daa4dba4c2d7a943e8a99897a5
Moxvallix Mox <moxvallix@gmail.com>
2025-12-14 05:04:36 +1030

    Revert "chore: add polyfill for tippy.js"
    This reverts commit 07c9b2bbd09b65445e1da7e185538c3d2ea018f4.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import "context-filter-polyfill";
 
-// Polyfill for tippy.js
-window.process = { env: { NODE_ENV: 'development' }};
-
 import Editor from "./editor/main.js";
 import UI from "./ui/main.js";
 import ProjectLoader from "./editor/format/project_loader.js";
