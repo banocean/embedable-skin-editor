07c9b2bbd09b65445e1da7e185538c3d2ea018f4
Moxvallix Mox <moxvallix@gmail.com>
2025-12-14 05:03:13 +1030

    chore: add polyfill for tippy.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import "context-filter-polyfill";
 
+// Polyfill for tippy.js
+window.process = { env: { NODE_ENV: 'development' }};
+
 import Editor from "./editor/main.js";
 import UI from "./ui/main.js";
 import ProjectLoader from "./editor/format/project_loader.js";
